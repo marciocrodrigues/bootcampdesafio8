c2b5ed77b39736ede13318d1919ab71b7fd43001
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2020-07-12 18:37:27 -0300

    correções

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,31 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const cartAmount = products.reduce((accumulator, product) => {
+      const total = accumulator + product.price * product.quantity;
 
-    return formatValue(0);
+      return total;
+    }, 0);
+
+    return formatValue(cartAmount);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const cartItems = products.reduce((accumulator, product) => {
+      const total = accumulator + product.quantity;
 
-    return 0;
+      return total;
+    }, 0);
+
+    return cartItems;
   }, [products]);
 
   return (
